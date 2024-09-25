1ca06be8dafa77e166e3b90620d07a40e434b1c1
Savindu <savinduruhunuhewa@gmail.com>
2024-09-25 15:59:19 +0530

    Check the data types of the dataset

--- data-analysis.R
+++ data-analysis.R
@@ -58,6 +58,9 @@ data_new <- read.csv("data_new.csv")
 ## View the  new datset
 View(data_new)
 
+## check the data types  and number of objects
+str(data_new)
+
 ## Normality testing
 ## install the nortest package
 install.packages("nortest")
